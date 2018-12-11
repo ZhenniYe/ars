296668a038fa8968e4d4c28a6d540d8105acc001
yantingpan <yanting_pan@berkeley.edu>
2018-12-11 12:24:10 -0800

    add utils test

--- ars/tests/testthat/test-utils.R
+++ ars/tests/testthat/test-utils.R
@@ -5,7 +5,9 @@ set.seed(2018)
 square_test <- function(x){return(x^2)}
 
 
+
 test_that("Test `calcDeriv()` function",{
+  
   expect_equal(calcDeriv(dnorm, 0), 0)
   expect_equal(round(calcDeriv(square_test,1)), 2)
 
@@ -13,7 +15,9 @@ test_that("Test `calcDeriv()` function",{
 })
 
 
+
 test_that("Test `lowerPDF()` function",{
+  
   expect_equal(lowerPDF(dnorm, 100, c(-10, 10)), 0)
   expect_equal(lowerPDF(dnorm, -11, c(-10, 10)), 0)
   expect_is(lowerPDF(dnorm, 2, c(-10, 10)), "numeric")
@@ -25,10 +29,38 @@ test_that("Test `lowerPDF()` function",{
 })
 
 
-test_that("Test `expPDF()` function",{
 
-
-  cat('\n`expPDF()` function tests passed`\n')
+test_that("Test `expCDF()` function",{
+  
+  expect_equal(expCDF(c(-2,-1), c(-2,0), -1, c(2,0), c(-2.9, -0.9))$cdf[1], 0)
+  expect_equal(expCDF(c(-2,-1), c(-2,0), -1, c(2,0), c(-2.9, -0.9))$cdf[2], 1)
+  expect_equal(round(expCDF(c(-2,-1), c(-2,0), -1, c(2,0), c(-2.9, -0.9))$norm.const, 3), 0.176)
+  expect_true(expCDF(1, c(0.09,0.99), 1, c(-1,-100), c(0.6, -4))$cdf == "NaN")
+  
+  cat('\n`expCDF()` function tests passed`\n')
 })
 
 
+
+test_that("Test `invCDF()` function",{
+  
+  expect_equal(round(invCDF(1, 1, c(0, -2), c(-0.919, 1.08),  0.571, c(0, -0.600), 0), 3), 1.964)
+  expect_equal(round(invCDF(0.2, 1, c(0, -2), c(-0.919, 1.08),  0.571, c(0, -0.600), 0), 3), 0.286)
+  expect_equal(round(invCDF(1, -1, c(2, 0), c(-2.919, -0.919), 0.571, c(0, -0.598), 0.027)), 0)
+  expect_equal(round(invCDF(-0.14, 0, c(1, -1), c(-0.419, -0.419),  0.832, c(0, -1.315), 0.242), 3), -1.656)
+  
+  cat('\n`invCDF()` function tests passed`\n')
+})
+
+
+
+test_that("Test `setParams()` function",{
+  
+  expect_is(setParams(dnorm, -2, 0, c(-2,0))$m_p[1], "numeric")
+  expect_equal(round(setParams(dnorm, -2, 0, c(-2,0))$m_p[1]), 2)
+  expect_equal(setParams(dnorm, -2, 0, c(-2,0))$m_p[2], 0)
+  expect_equal(round(setParams(dnorm, -2, 0, c(-2,0))$int_x), -1)
+  expect_equal(setParams(dnorm, -2, 0, c(-2,0))$shift[1], 0)
+  
+  cat('\n`setParams()` function tests passed`\n')
+})
